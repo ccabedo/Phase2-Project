19cd03e37a4ee3eab4ba6c729c9de2037b974845
Colleen Cabedo <cmcabedo@gmail.com>
2022-10-24 11:43:05 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Map from './Map';
+
+function App () {
+    return(
+    <>
+        <Header />
+        <Map />
+    </>
+    );
+}
+
+export default App;
